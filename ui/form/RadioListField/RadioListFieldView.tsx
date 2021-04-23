a37b7db7bb892dc59c4d0f91fd5e07aea5a1974f
SergeiKaluckij <sergkaluckij@gmail.com>
2021-04-23 13:46:01 +0700

    fix radio list

--- ui/form/RadioListField/RadioListFieldView.tsx
+++ ui/form/RadioListField/RadioListFieldView.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import {Text, View} from "react-native";
-import {bem} from "@steroidsjs/core/hoc";
-import {IBemHocOutput} from "@steroidsjs/core/hoc/bem";
-import {IRadioListFieldViewProps} from "@steroidsjs/core/ui/form/RadioListField/RadioListField";
-import styles from './RadioFieldListViewStyles'
-import Touchable from "../../../utils/Touchable";
+import React from 'react';
+import { Text, View } from 'react-native';
+import { bem } from '@steroidsjs/core/hoc';
+import { IBemHocOutput } from '@steroidsjs/core/hoc/bem';
+import { IRadioListFieldViewProps } from '@steroidsjs/core/ui/form/RadioListField/RadioListField';
+import styles from './RadioFieldListViewStyles';
+import Touchable from '../../../utils/Touchable';
 
 @bem('RadioListFieldView')
 export default class RadioListFieldView extends React.PureComponent <IRadioListFieldViewProps & IBemHocOutput> {
@@ -12,14 +12,16 @@ export default class RadioListFieldView extends React.PureComponent <IRadioListF
         const bem = this.props.bem;
         return (
             <View style={bem.block()}>
-                {this.props.items.map(item => (
-                    <Touchable onPress={() => this.props.onItemClick(item)}>
-                        <View style={bem(bem.element('item'), item.isSelected && {backgroundColor: '#0084FF'})}>
-                            <Text style={bem.element('label')}>{item.label}</Text>
-                        </View>
-                    </Touchable>
-                ))}
+                <View style={bem.element('group')}>
+                    {this.props.items.map(item => (
+                        <Touchable onPress={() => this.props.onItemClick(item)}>
+                            <View style={bem(bem.element('item'), item.isSelected && {backgroundColor: '#0084FF'})}>
+                                <Text style={bem.element('label')}>{item.label}</Text>
+                            </View>
+                        </Touchable>
+                    ))}
+                </View>
             </View>
-        )
+        );
     }
 }
