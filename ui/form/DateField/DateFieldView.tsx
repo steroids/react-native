4b2be0ca35f2f9993e229ae8015d6c0b813a8e77
dmartynov <denis.martynov.93@mail.ru>
2021-06-04 22:40:44 +0700

    2.0.0-beta.42, remove touchable highlight in DateFieldView

--- ui/form/DateField/DateFieldView.tsx
+++ ui/form/DateField/DateFieldView.tsx
@@ -7,11 +7,11 @@ import {Platform} from 'react-native';
 
 import {
     StyleProp,
+    TouchableWithoutFeedback,
     View
 } from "react-native";
 import {IDateFieldViewProps} from "@steroidsjs/core/ui/form/DateField/DateField";
 import Icon from "@steroidsjs/core/ui/icon/Icon";
-import Touchable from '../../../utils/Touchable';
 
 interface IProps extends IDateFieldViewProps, IBemHocOutput {
     required: boolean,
@@ -78,11 +78,11 @@ export default class DateFieldView extends React.PureComponent<IProps, IState> {
         const bem = this.props.bem;
         return (
             <View style={bem(bem.block(), this.props.style)}>
-                <Touchable
+                <TouchableWithoutFeedback
                     style={bem(bem.element('input'))}
                     onPress={() => !this.props.disabled && this.togglePicker()}
                 >
-                    <View pointerEvents='none'>
+                    <View pointerEvents='box-only'>
                         <InputFieldView
                             editable={false}
                             placeholder={this.props.placeholder}
@@ -99,7 +99,7 @@ export default class DateFieldView extends React.PureComponent<IProps, IState> {
                             inputProps={this.props.inputProps}
                         />
                     </View>
-                </Touchable>
+                </TouchableWithoutFeedback>
 
                 {this.state.showPicker && (
                     <DateTimePicker
