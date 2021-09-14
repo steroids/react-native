7232c8633c112dea04b58dc900f2e3fac8f7a0c4
skaluckij <sergkaluckij@gmail.com>
2021-09-14 17:50:31 +0700

    AutoCompleteFieldView changed to functional component

--- src/ui/form/AutoCompleteField/AutoCompleteFieldView.tsx
+++ src/ui/form/AutoCompleteField/AutoCompleteFieldView.tsx
@@ -1,73 +1,70 @@
 import * as React from 'react';
-import bem, { IBemHocOutput } from '../../../hoc/bemNative';
 import InputFieldView from '../InputField/InputFieldView';
 import OptionsList from '../../shared/OptionsList';
 import { TouchableWithoutFeedback, View } from 'react-native';
 import { IAutoCompleteFieldViewProps } from '@steroidsjs/core/ui/form/AutoCompleteField/AutoCompleteField';
+import useBemNative from '../../../hooks/useBemNative';
 
-interface IProps extends IAutoCompleteFieldViewProps, IBemHocOutput {
+interface IProps extends IAutoCompleteFieldViewProps {
     style: any;
     items: any;
     inputProps: any;
 }
 
-@bem('AutoCompleteFieldView')
-export default class AutoCompleteFieldView extends React.PureComponent<IProps> {
+const AutoCompleteFieldView: React.FunctionComponent<IProps> = (props) => {
+    const bem = useBemNative('AutoCompleteFieldView');
 
-    static defaultProps = {
-        required: false,
-        placeholder: null,
-        isInvalid: false,
-        disabled: false,
-        size: 'md',
-    };
+    // TODO wtf? it should work without this
+    const itemsFormatted = React.useMemo(() => props.items.map(item => {
+        item.id = item.label;
+        return item;
+    }), [props.items]);
 
-    render() {
-        // TODO wtf? it should work without this
-        const itemsFormatted = this.props.items.map(item => {
-            item.id = item.label;
-            return item;
-        });
-
-        const bem = this.props.bem;
-        // @ts-ignore
-        // @ts-ignore
-        return (
-            <View style={bem(bem.block(), this.props.style)}>
-                <TouchableWithoutFeedback
-                    style={bem(bem.element('input'))}
-                    onPress={() => !this.props.disabled && this.props.onOpen()}
-                >
-                    <View>
-                        <InputFieldView
-                            autoFocus={false}
-                            {...this.props.inputProps}
-                            placeholder={this.props.placeholder}
-                            disabled={this.props.disabled}
-                            isInvalid={this.props.isInvalid}
-                            required={this.props.required}
-                            editable={false}
-                            size={this.props.size}
-                            inputProps={null}
-                        />
-                    </View>
-                </TouchableWithoutFeedback>
-
-                {this.props.isOpened && (
-                    <OptionsList
-                        items={itemsFormatted}
-                        selectedItems={this.props.selectedItems}
-                        onClose={this.props.onClose}
-                        onItemClick={item => this.props.onItemClick(item)}
-                        searchInputProps={this.props.inputProps}
-                        placeholderText={this.props.placeholder || __('Начните вводить название...')}
-                        noResultsText={__('Нет совпадений, введите свое значение')}
-                        selectRequired={false}
-                        appearanceType={'fade'}
-                        focusInputOnOpen={true}
+    return (
+        <View style={bem(bem.block(), props.style)}>
+            <TouchableWithoutFeedback
+                style={bem(bem.element('input'))}
+                onPress={() => !props.disabled && props.onOpen()}
+            >
+                <View>
+                    <InputFieldView
+                        autoFocus={false}
+                        {...props.inputProps}
+                        placeholder={props.placeholder}
+                        disabled={props.disabled}
+                        isInvalid={props.isInvalid}
+                        required={props.required}
+                        editable={false}
+                        size={props.size}
+                        inputProps={null}
                     />
-                )}
-            </View>
-        );
-    }
-}
+                </View>
+            </TouchableWithoutFeedback>
+
+            {props.isOpened && (
+                <OptionsList
+                    items={itemsFormatted}
+                    selectedItems={props.selectedItems}
+                    onClose={props.onClose}
+                    onItemClick={item => props.onItemClick(item)}
+                    searchInputProps={props.inputProps}
+                    placeholderText={props.placeholder || __('Начните вводить название...')}
+                    noResultsText={__('Нет совпадений, введите свое значение')}
+                    selectRequired={false}
+                    appearanceType={'fade'}
+                    focusInputOnOpen={true}
+                />
+            )}
+        </View>
+    );
+};
+
+AutoCompleteFieldView.defaultProps = {
+    required: false,
+    placeholder: null,
+    isInvalid: false,
+    disabled: false,
+    size: 'md',
+};
+
+export default AutoCompleteFieldView;
