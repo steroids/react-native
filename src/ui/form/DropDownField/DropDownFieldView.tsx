020ce2ceb5d01a79de1e75ac69c4263e823aada9
skaluckij <sergkaluckij@gmail.com>
2021-09-14 17:50:11 +0700

    DropDownFieldView changed to functional component

--- src/ui/form/DropDownField/DropDownFieldView.tsx
+++ src/ui/form/DropDownField/DropDownFieldView.tsx
@@ -1,101 +1,99 @@
 import * as React from 'react';
-import bem, { IBemHocOutput } from '../../../hoc/bemNative';
 import { IDropDownFieldViewProps } from '@steroidsjs/core/ui/form/DropDownField/DropDownField';
 import InputFieldView from '../InputField/InputFieldView';
 import OptionsList from '../..//shared/OptionsList';
-
 import {
     Image, TextInputProps, TouchableNativeFeedback,
     TouchableWithoutFeedback,
     View,
 } from 'react-native';
+import useBemNative from '../../../hooks/useBemNative';
 
-interface IProps extends IDropDownFieldViewProps, IBemHocOutput {
+interface IProps extends IDropDownFieldViewProps {
     inputProps: TextInputProps,
     items: any,
 }
 
-@bem('DropDownFieldView')
-export default class DropDownFieldView extends React.PureComponent<IProps> {
+const DropDownFieldView: React.FunctionComponent<IProps> = (props) => {
+    const bem = useBemNative('DropDownFieldView');
 
-    static defaultProps = {
-        label: null,
-        hint: null,
-        required: false,
-        placeholder: null,
-        isInvalid: false,
-        searchPlaceholder: null,
-        size: 'md',
-        disabled: false,
-        className: '',
-        searchInputProps: null,
-        searchAutoFocus: false,
-        multiple: false,
-        autoComplete: false,
-        inputProps: null,
-    };
-
-    renderArrowIcon() {
-        const bem = this.props.bem;
+    const renderArrowIcon = () => {
+        const bem = props.bem;
         return (
             <Image
                 style={bem(bem.element('arrow-icon'))}
                 source={require('../../../assets/arrow-down.png')}
             />
         );
-    }
+    };
 
-    renderResetIcon() {
-        const bem = this.props.bem;
+    const renderResetIcon = () => {
+        const bem = props.bem;
         return (
-            <TouchableNativeFeedback onPress={() => this.props.onReset()}>
+            <TouchableNativeFeedback onPress={() => props.onReset()}>
                 <Image
                     style={bem(bem.element('reset-icon'))}
                     source={require('../../../assets/close-icon.png')}
                 />
             </TouchableNativeFeedback>
         );
-    }
+    };
 
-    render() {
-        const bem = this.props.bem;
-        return (
-            <View style={bem(bem.block(), this.props.style)}>
-                <TouchableWithoutFeedback
-                    style={bem(bem.element('input'))}
-                    onPress={() => !this.props.disabled && this.props.onOpen()}
-                >
-                    <View>
-                        <InputFieldView
-                            autoFocus={false}
-                            editable={false}
-                            placeholder={this.props.placeholder}
-                            suffixElement={
-                                this.props.selectedItems.length > 0 && this.props.showReset
-                                    ? this.renderResetIcon()
-                                    : this.renderArrowIcon()
-                            }
-                            size={this.props.size}
-                            value={this.props.selectedItems.map(item => item.label).join((' '))}
-                            isInvalid={this.props.isInvalid}
-                            disabled={this.props.disabled}
-                            inputProps={this.props.inputProps}
-                        />
-                    </View>
-                </TouchableWithoutFeedback>
-
-                {this.props.isOpened && (
-                    <OptionsList
-                        items={this.props.items}
-                        selectedItems={this.props.selectedItems}
-                        onClose={this.props.onClose}
-                        onItemClick={item => this.props.onItemClick(item)}
-                        searchInputProps={this.props.searchInputProps}
-                        inputFieldIcon={require('../../../assets/search-icon.png')}
-                        appearanceType={'fade'}
+    return (
+        <View style={bem(bem.block(), props.style)}>
+            <TouchableWithoutFeedback
+                style={bem(bem.element('input'))}
+                onPress={() => !props.disabled && props.onOpen()}
+            >
+                <View>
+                    <InputFieldView
+                        autoFocus={false}
+                        editable={false}
+                        placeholder={props.placeholder}
+                        suffixElement={
+                            props.selectedItems.length > 0 && props.showReset
+                                ? renderResetIcon()
+                                : renderArrowIcon()
+                        }
+                        size={props.size}
+                        value={props.selectedItems.map(item => item.label).join((' '))}
+                        isInvalid={props.isInvalid}
+                        disabled={props.disabled}
+                        inputProps={props.inputProps}
                     />
-                )}
-            </View>
-        );
-    }
-}
+                </View>
+            </TouchableWithoutFeedback>
+
+            {props.isOpened && (
+                <OptionsList
+                    items={props.items}
+                    selectedItems={props.selectedItems}
+                    onClose={props.onClose}
+                    onItemClick={item => props.onItemClick(item)}
+                    searchInputProps={props.searchInputProps}
+                    inputFieldIcon={require('../../../assets/search-icon.png')}
+                    appearanceType={'fade'}
+                />
+            )}
+        </View>
+    );
+};
+
+DropDownFieldView.defaultProps = {
+    label: null,
+    hint: null,
+    required: false,
+    placeholder: null,
+    isInvalid: false,
+    searchPlaceholder: null,
+    size: 'md',
+    disabled: false,
+    className: '',
+    searchInputProps: null,
+    searchAutoFocus: false,
+    multiple: false,
+    autoComplete: false,
+    inputProps: null,
+};
+
+export default DropDownFieldView;
