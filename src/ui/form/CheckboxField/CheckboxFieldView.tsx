4869c4c8ed3d72d46fd45f3016a1c2e1cdf4698c
skaluckij <sergkaluckij@gmail.com>
2021-09-14 13:26:55 +0700

    CheckboxFieldView changed to functional component

--- src/ui/form/CheckboxField/CheckboxFieldView.tsx
+++ src/ui/form/CheckboxField/CheckboxFieldView.tsx
@@ -1,43 +1,41 @@
 import * as React from 'react';
 import { Switch, Text, View, TextProps } from 'react-native';
-import bem, { IBemHocOutput } from '../../../hoc/bemNative';
 import { ICheckboxFieldProps } from '@steroidsjs/core/ui/form/CheckboxField/CheckboxField';
 import Touchable from '../../../utils/Touchable/Touchable';
+import useBemNative from '../../../hooks/useBemNative';
 
-interface IProps extends ICheckboxFieldProps, IBemHocOutput {
+interface IProps extends ICheckboxFieldProps {
     stylesContainer: any
     stylesText: any
     textProps: TextProps,
 }
 
-@bem('CheckboxFieldView')
-export default class CheckboxFieldView extends React.PureComponent <IProps> {
-    render() {
-        const bem = this.props.bem;
-        const {onChange, checked} = this.props.inputProps;
-        return (
-            <View style={bem(bem.block(), this.props.stylesContainer)}>
-                <Switch
-                    disabled={this.props.disabled}
-                    trackColor={{
-                        false: bem.variables('trackSelectedFalseBg'),
-                        true: bem.variables('trackSelectedTrueBg'),
-                    }}
-                    thumbColor={checked ? bem.variables('thumbSelectedTrueBg') : bem.variables('thumbSelectedFalseBg')}
-                    onValueChange={onChange}
-                    value={checked}
-                />
-                <Touchable
-                    style={bem.element('label')}
-                    onPress={() => onChange(!checked)}
-                >
-                    <View style={bem.element('text-wrapper')}>
-                        <Text {...this.props.textProps} style={bem(bem.element('label-text'), this.props.stylesText)}>
-                            {this.props.label}
-                        </Text>
-                    </View>
-                </Touchable>
-            </View>
-        );
-    }
-}
+const CheckboxFieldView: React.FunctionComponent<IProps> = (props) => {
+    const bem = useBemNative('CheckboxFieldView');
+    return (
+        <View style={bem(bem.block(), props.stylesContainer)}>
+            <Switch
+                disabled={props.disabled}
+                trackColor={{
+                    false: bem.variables('trackSelectedFalseBg'),
+                    true: bem.variables('trackSelectedTrueBg'),
+                }}
+                thumbColor={props.inputProps.checked ? bem.variables('thumbSelectedTrueBg') : bem.variables('thumbSelectedFalseBg')}
+                onValueChange={props.inputProps.onChange}
+                value={props.inputProps.checked}
+            />
+            <Touchable
+                style={bem.element('label')}
+                onPress={() => props.inputProps.onChange(!props.inputProps.checked)}
+            >
+                <View style={bem.element('text-wrapper')}>
+                    <Text {...props.textProps} style={bem(bem.element('label-text'), props.stylesText)}>
+                        {props.label}
+                    </Text>
+                </View>
+            </Touchable>
+        </View>
+    );
+};
+
+export default CheckboxFieldView;
