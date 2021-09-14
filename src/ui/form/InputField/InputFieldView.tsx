ff091dc5e490553ac7d92530ff4d33a9342133a4
skaluckij <sergkaluckij@gmail.com>
2021-09-14 13:23:43 +0700

    InputFieldView changed to functional component

--- src/ui/form/InputField/InputFieldView.tsx
+++ src/ui/form/InputField/InputFieldView.tsx
@@ -1,14 +1,14 @@
 import React, { ReactNode } from 'react';
 import { Image, ImageSourcePropType, StyleProp, TextInput, View } from 'react-native';
-import bem, { IBemHocOutput } from '../../../hoc/bemNative';
 import { IInputFieldViewProps } from '@steroidsjs/core/ui/form/InputField/InputField';
 import styles from './InputFieldViewStyles';
+import useBemNative from '../../../hooks/useBemNative';
 
 type KeyboardTypes = 'default' | 'number-pad' | 'decimal-pad' | 'numeric' | 'email-address' | 'phone-pad';
 type AutoCompleteTypes = 'off' | 'username' | 'password' | 'email' | 'name' | 'tel' | 'street-address' |
     'postal-code' | 'cc-number' | 'cc-csc' | 'cc-exp' | 'cc-exp-month' | 'cc-exp-year';
 
-interface IRNInputFieldViewProps extends IInputFieldViewProps, IBemHocOutput {
+interface IRNInputFieldViewProps extends IInputFieldViewProps {
     keyboardType?: KeyboardTypes,
     autoCompleteType?: AutoCompleteTypes,
     size?: Size,
@@ -29,118 +29,106 @@ interface IState {
     focused: Boolean;
 }
 
-@bem('InputFieldView', styles)
-export default class InputFieldView extends React.PureComponent<IRNInputFieldViewProps, IState> {
+const InputFieldView: React.FunctionComponent<IRNInputFieldViewProps> = (props) => {
+    const bem = useBemNative('InputFieldView', styles);
+    const [state, setState] = React.useState<IState>({focused: false});
 
-    constructor(props: any) {
-        super(props);
-
-        this.state = {
-            focused: false,
-        };
-    }
-
-    static defaultProps = {
-        type: 'text',
-        disabled: false,
-        required: false,
-        className: '',
-        placeholder: '',
-        errors: [],
-        size: 'md',
-        keyboardType: 'default',
-        autoCompleteType: 'off',
-        prefixElement: null,
-        suffixElement: null,
-        multiline: false,
-        editable: true,
-        value: null,
-        autoFocus: false,
-    };
-
-    renderSideAddonElement(component: ImageSourcePropType | ReactNode, element: 'before' | 'after') {
-        const bem = this.props.bem;
-
+    const renderSideAddonElement = (component: ImageSourcePropType | ReactNode, element: 'before' | 'after') => {
         if (React.isValidElement(component) || typeof component === 'function') {
             return (
-                <View style={bem.element('addon', {element, size: this.props.size})}>
+                <View style={bem.element('addon', {element, size: props.size})}>
                     {component}
                 </View>
             );
         }
 
         return (
-            <View style={bem.element('addon', {element, size: this.props.size})}>
+            <View style={bem.element('addon', {element, size: props.size})}>
                 <Image
-                    style={bem(bem.element('side-element', {size: this.props.size}))}
+                    style={bem(bem.element('side-element', {size: props.size}))}
                     source={component as ImageSourcePropType}
                 />
             </View>
         );
-    }
+    };
 
-    renderSideTextElement(component: ImageSourcePropType | ReactNode) {
-        const bem = this.props.bem;
-
+    const renderSideTextElement = (component: ImageSourcePropType | ReactNode) => {
         if (React.isValidElement(component) || typeof component === 'function') {
             return component;
         }
 
         return (
             <Image
-                style={bem(bem.element('side-element', {size: this.props.size}))}
+                style={bem(bem.element('side-element', {size: props.size}))}
                 source={component as ImageSourcePropType}
             />
         );
-    }
+    };
 
-    render() {
-        const bem = this.props.bem;
-        return (
-            <View
-                style={bem(
-                    bem.block({
-                        size: this.props.size,
-                        invalid: this.props.isInvalid,
-                        disabled: this.props.disabled,
-                        focused: this.state.focused,
-                    }),
-                    this.props.style,
-                )}
-            >
-                {this.props.addonBefore && this.renderSideAddonElement(this.props.addonBefore, 'before')}
-                {this.props.textBefore && this.renderSideTextElement(this.props.textBefore)}
-                <View style={bem.element('input-text', {size: this.props.size})}>
-                    <TextInput
-                        style={bem(
-                            bem.element('input', {
-                                invalid: this.props.isInvalid,
-                                disabled: this.props.disabled,
-                                size: this.props.size,
-                                multiline: Boolean(this.props.multiline),
-                            }),
-                        )}
-                        placeholderTextColor={bem.variables('inputPlaceholderColor')}
-                        {...this.props.inputProps}
-                        secureTextEntry={this.props.type === 'password'}
-                        autoFocus={this.props.autoFocus}
-                        autoCompleteType={this.props.autoCompleteType}
-                        keyboardType={this.props.keyboardType}
-                        placeholder={this.props.placeholder}
-                        editable={this.props.editable && !this.props.disabled}
-                        onFocus={() => this.setState({focused: true})}
-                        onBlur={() => this.setState({focused: false})}
-                        onChange={(e) => {
-                            this.props.inputProps.onChange(e.nativeEvent.text);
-                        }}
-                        value={this.props.value && String(this.props.value)}
-                        numberOfLines={Number(this.props.multiline) || 1}
-                        multiline={this.props.multiline && this.props.multiline > 1}
-                    />
-                </View>
-                {this.props.textAfter && this.renderSideTextElement(this.props.textAfter)}
-                {this.props.addonAfter && this.renderSideAddonElement(this.props.addonAfter, 'after')}
+    return (
+        <View
+            style={bem(
+                bem.block({
+                    size: props.size,
+                    invalid: props.isInvalid,
+                    disabled: props.disabled,
+                    focused: state.focused,
+                }),
+                props.style,
+            )}
+        >
+            {props.addonBefore && renderSideAddonElement(props.addonBefore, 'before')}
+            {props.textBefore && renderSideTextElement(props.textBefore)}
+            <View style={bem.element('input-text', {size: props.size})}>
+                <TextInput
+                    style={bem(
+                        bem.element('input', {
+                            invalid: props.isInvalid,
+                            disabled: props.disabled,
+                            size: props.size,
+                            multiline: Boolean(props.multiline),
+                        }),
+                    )}
+                    placeholderTextColor={bem.variables('inputPlaceholderColor')}
+                    {...props.inputProps}
+                    secureTextEntry={props.type === 'password'}
+                    autoFocus={props.autoFocus}
+                    autoCompleteType={props.autoCompleteType}
+                    keyboardType={props.keyboardType}
+                    placeholder={props.placeholder}
+                    editable={props.editable && !props.disabled}
+                    onFocus={() => setState({focused: true})}
+                    onBlur={() => setState({focused: false})}
+                    onChange={(e) => {
+                        props.inputProps.onChange(e.nativeEvent.text);
+                    }}
+                    value={props.value && String(props.value)}
+                    numberOfLines={Number(props.multiline) || 1}
+                    multiline={props.multiline && props.multiline > 1}
+                />
             </View>
-        );
-    }
-}
+            {props.textAfter && renderSideTextElement(props.textAfter)}
+            {props.addonAfter && renderSideAddonElement(props.addonAfter, 'after')}
+        </View>
+    );
+};
+
+InputFieldView.defaultProps = {
+    type: 'text',
+    disabled: false,
+    required: false,
+    className: '',
+    placeholder: '',
+    errors: [],
+    size: 'md',
+    keyboardType: 'default',
+    autoCompleteType: 'off',
+    prefixElement: null,
+    suffixElement: null,
+    multiline: false,
+    editable: true,
+    value: null,
+    autoFocus: false,
+};
+
+export default InputFieldView;
