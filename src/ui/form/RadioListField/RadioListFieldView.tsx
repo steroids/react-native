76ce340ac70a3ef25f60c17d3e07161ce8528bc7
skaluckij <sergkaluckij@gmail.com>
2021-09-14 13:30:44 +0700

    RadioListFieldView changed to functional component

--- src/ui/form/RadioListField/RadioListFieldView.tsx
+++ src/ui/form/RadioListField/RadioListFieldView.tsx
@@ -1,34 +1,33 @@
 import * as React from 'react';
 import { Text, View, TextProps } from 'react-native';
-import bem, { IBemHocOutput } from '../../../hoc/bemNative';
 import { IRadioListFieldViewProps } from '@steroidsjs/core/ui/form/RadioListField/RadioListField';
 import styles from './RadioFieldListViewStyles';
 import Touchable from '../../../utils/Touchable';
+import useBemNative from '../../../hooks/useBemNative';
 
-interface IProps extends IRadioListFieldViewProps, IBemHocOutput {
+interface IProps extends IRadioListFieldViewProps {
     textProps: TextProps,
     onItemClick: (item: any) => void,
 }
 
-@bem('RadioListFieldView', styles)
-export default class RadioListFieldView extends React.PureComponent <IProps> {
-    render() {
-        const bem = this.props.bem;
-        return (
-            <View style={bem.block()}>
-                <View style={bem.element('group')}>
-                    {this.props.items.map(item => (
-                        <Touchable
-                            key={item.id.toString()}
-                            onPress={() => this.props.onItemClick(item)}
-                        >
-                            <View style={bem(bem.element('item'), item.isSelected && {backgroundColor: '#0084FF'})}>
-                                <Text {...this.props.textProps} style={bem.element('label')}>{item.label}</Text>
-                            </View>
-                        </Touchable>
-                    ))}
-                </View>
+const RadioListFieldView: React.FunctionComponent<IProps> = (props) => {
+    const bem = useBemNative('RadioListFieldView', styles);
+    return (
+        <View style={bem.block()}>
+            <View style={bem.element('group')}>
+                {props.items.map(item => (
+                    <Touchable
+                        key={item.id.toString()}
+                        onPress={() => props.onItemClick(item)}
+                    >
+                        <View style={bem(bem.element('item'), item.isSelected && {backgroundColor: '#0084FF'})}>
+                            <Text {...props.textProps} style={bem.element('label')}>{item.label}</Text>
+                        </View>
+                    </Touchable>
+                ))}
             </View>
-        );
-    }
-}
+        </View>
+    );
+};
+
+export default RadioListFieldView;
