da89fdf6c3894a33e546ca1b3f12580117dace9e
skaluckij <sergkaluckij@gmail.com>
2021-09-20 14:03:25 +0700

    add secure storage and new version publish

--- src/components/ClientStorageComponent.ts
+++ src/components/ClientStorageComponent.ts
@@ -1,4 +1,5 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import EncryptedStorage from 'react-native-encrypted-storage';
 
 export const STORAGE_SECURE = 'secure';
 export const STORAGE_ASYNC = 'async';
@@ -10,20 +11,16 @@ export default class ClientStorageComponent {
      * @param {string} [storageName]
      * @returns {*}
      */
-    async get(name, storageName = STORAGE_SECURE) {
-        // if (storageName === STORAGE_SECURE) {
-        //     return await SecureStore.getItemAsync(name);
-        // }
-
-        if (storageName === STORAGE_ASYNC) {
-            try {
-                return await AsyncStorage.getItem(name)
-            } catch(e) {
-                // read error
+    async get(name, storageName = STORAGE_ASYNC) {
+        switch (storageName) {
+            case STORAGE_SECURE: {
+                return EncryptedStorage.getItem(name);
+            }
+            case STORAGE_ASYNC:
+            default: {
+                return AsyncStorage.getItem(name);
             }
         }
-
-        return null;
     }
 
     /**
@@ -33,16 +30,14 @@ export default class ClientStorageComponent {
      * @param {number|null} [expires]
      * TODO expires isn't supported by SecureStore
      */
-    async set(name, value, storageName = STORAGE_SECURE, expires = null) {
-        // if (storageName === STORAGE_SECURE) {
-        //     await SecureStore.setItemAsync(name, value);
-        // }
-
-        if (storageName === STORAGE_ASYNC) {
-            try {
-                await AsyncStorage.setItem(name, value)
-            } catch(e) {
-                // save error
+    async set(name, value, storageName = STORAGE_ASYNC, expires = null) {
+        switch (storageName) {
+            case STORAGE_SECURE: {
+                return EncryptedStorage.setItem(name, value);
+            }
+            case STORAGE_ASYNC:
+            default: {
+                return AsyncStorage.setItem(name, value);
             }
         }
     }
@@ -51,16 +46,14 @@ export default class ClientStorageComponent {
      * @param {string} name
      * @param {string} [storageName]
      */
-    async remove(name, storageName = STORAGE_SECURE) {
-        // if (storageName === STORAGE_SECURE) {
-        //     await SecureStore.deleteItemAsync(name);
-        // }
-
-        if (storageName === STORAGE_ASYNC) {
-            try {
-                await AsyncStorage.removeItem(name)
-            } catch(e) {
-                // remove error
+    async remove(name, storageName = STORAGE_ASYNC) {
+        switch (storageName) {
+            case STORAGE_SECURE: {
+                return EncryptedStorage.removeItem(name);
+            }
+            case STORAGE_ASYNC:
+            default: {
+                return AsyncStorage.removeItem(name);
             }
         }
     }
@@ -71,28 +64,25 @@ export default class ClientStorageComponent {
      * TODO SecureStore API doesn't include clear all method
      */
     async clear(storageName = STORAGE_SECURE, clearKeys = []) {
-        if (storageName === STORAGE_SECURE) {
-            if (clearKeys.length) {
-                clearKeys.forEach(key => {
-                    this.remove(key);
-                });
-            } else {
-                //
-                console.warn('No keys were found for the cleaning all keys');
+        switch (storageName) {
+            case STORAGE_SECURE: {
+                if (clearKeys.length) {
+                    clearKeys.forEach(key => {
+                        this.remove(key, STORAGE_SECURE);
+                    });
+                } else {
+                    await EncryptedStorage.clear();
+                }
+                break;
             }
-            return;
-        }
-
-        if (storageName === STORAGE_ASYNC) {
-            if (clearKeys.length) {
-                clearKeys.forEach(key => {
-                    this.remove(key, STORAGE_ASYNC);
-                });
-            } else {
-                try {
-                    await AsyncStorage.clear()
-                } catch(e) {
-                    // clear error
+            case STORAGE_ASYNC:
+            default: {
+                if (clearKeys.length) {
+                    clearKeys.forEach(key => {
+                        this.remove(key);
+                    });
+                } else {
+                    await AsyncStorage.clear();
                 }
             }
         }
