ef0e90245e6657f6881ba07341f39edfe3e19b2a
Konstantin Smirnov <smirnov.konstantiin@gmail.com>
2026-04-01 15:54:59 +0700

    fix errors

--- src/ui/layout/Notifications/NotificationItemView.tsx
+++ src/ui/layout/Notifications/NotificationItemView.tsx
@@ -13,18 +13,18 @@ export default class NotificationItemView extends React.Component<INotifications
     render() {
         const bem = this.props.bem;
 
-        const bgColor = bem.color(this.props.level);
+        const bgColor = bem.color(this.props.type);
         const lightColor = bem.color('white');
         const darkColor = bem.color('gray700');
         const colorText = getContrastColor(bgColor, lightColor, darkColor);
 
         return (
-            <View style={bem(bem.block(), 'bg-' + this.props.level)}>
+            <View style={bem(bem.block(), 'bg-' + this.props.type)}>
                 <Text style={bem(bem.element('text'), {color: colorText})}>{this.props.message}</Text>
                 <Button
                     style={bem.element('button', {color: colorText})}
                     size="sm"
-                    color={this.props.level}
+                    color={this.props.type}
                     onClick={this.props.onClose}
                     icon="closeIcon"
                     iconProps={{tintColor: colorText}}
